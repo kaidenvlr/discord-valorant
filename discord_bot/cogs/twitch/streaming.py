3d6c90026c0de54bd2e95bfc9441af622a22a511
kaidenvlr <rllntmt@gmail.com>
2023-01-20 15:14:47 +0600

    function streaming is working now

--- discord_bot/cogs/twitch/streaming.py
+++ discord_bot/cogs/twitch/streaming.py
@@ -22,24 +22,42 @@ class Stream(commands.Cog):
         delay = 60
         while not self.bot.is_closed():
             for guild in self.bot.guilds:
-                if guild.id == 1028539778272084039:
+                if guild.id == cfg.guild.guild_id:
                     # guild = await self.bot.fetch_guild(cfg.guild.guild_id)
                     for user in guild.get_role(cfg.roles.streamer).members:
-
-                        print(user.activity.__class__)
                         if type(user.activity) == discord.activity.Streaming:
                             description = f"{user.mention} сейчас стримит, скорее заходи!\n{user.activity.url}"
                             # description = f"{user.activity.url}"
                             activity_created_timestamp = user.activity.created_at.timestamp()
-                            now = datetime.datetime.fromtimestamp(datetime.datetime.now().timestamp() / 1000,
-                                                                  tz=datetime.timezone.utc).timestamp()
-                            print(activity_created_timestamp)
-                            print(now)
-                            last_online = ((now - activity_created_timestamp) * 1000) % 3600
-                            print(last_online)
+                            now = datetime.datetime.utcnow().timestamp()
+                            twitch_name = user.activity.assets['large_image'].split(':')[1]
+                            last_online = (now - activity_created_timestamp) % 3600
+                            print(datetime.datetime.fromtimestamp(now), last_online, sep="\t")
                             if 0 <= last_online < 60:
-                                await guild.system_channel.send(
-                                    embed=discord.Embed(description=description, url="https://www.go.twitch.tv/nix"))
+                                embed = discord.Embed(
+                                    title=user.activity.name,
+                                    url=user.activity.url
+                                )
+                                embed.add_field(
+                                    name="",
+                                    value=f"Играет в {user.activity.game}"
+                                )
+                                embed.set_thumbnail(
+                                    url=user.avatar.url
+                                )
+                                embed.set_image(
+                                    url=f"https://static-cdn.jtvnw.net/previews-ttv/live_user_{twitch_name}-1920x1080.jpg")
+                                embed.set_author(
+                                    name=user.name,
+                                    icon_url=user.avatar.url
+                                )
+                                announcement_channel = guild.get_channel(cfg.channels.stream_channel)
+                                announcement_role = guild.get_role(cfg.roles.twitch_announcement)
+                                await announcement_channel.send(
+                                    content=f'{announcement_role.mention}\n {user.mention} сейчас стримит, скорее '
+                                            f'заходи посмотреть!',
+                                    embed=embed
+                                )
 
                     await asyncio.sleep(delay)
 
