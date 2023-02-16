35a31c9bde2c989e3111cbb6687966229c2ed447
kaidenvlr <jzxcvblessed@gmail.com>
2023-02-16 19:58:21 +0600

    bot start file

--- bot.py
+++ bot.py
@@ -1,56 +1,122 @@
+from __future__ import annotations
+
+import asyncio
+import os
+import sys
+import traceback
+
+import aiohttp
 import discord
 from discord.ext import commands
+from discord.ext.commands import ExtensionFailed, ExtensionNotFound, NoEntryPointError
+from dotenv import load_dotenv
 
-from discord_bot.cogs.twitch.take_role import message
-from discord_bot.config import load_config
+from utils import locale_v2
+from utils.backup.db_fetch import fetch_user, fetch_notify
+from utils.valorant.cache import get_cache
 
-cfg = load_config(".env")
-cogs: list = [
-    "discord_bot.cogs.tracker.login",
-    "discord_bot.cogs.tracker.logout",
-    "discord_bot.cogs.tracker.shop",
-    # пока что работает сама через раз, не всегда получает response в нормальном и адекватном виде
+load_dotenv()
 
-    "discord_bot.cogs.tracker.stats",
-
-    "discord_bot.cogs.actions.actions",
-
-    "discord_bot.cogs.events.close.create",
-    # "discord_bot.cogs.events.new_member.new_member",
-
-    "discord_bot.cogs.twitch.streaming",
+initial_extensions = [
+    'cogs.actions',
+    'cogs.admin',
+    'cogs.create_close',
+    'cogs.errors',
+    'cogs.notify',
+    'cogs.streaming',
+    'cogs.valorant',
+    'cogs.tracker',
+    'cogs.profile',
+    # 'cogs.backup', # релизится
 ]
+
+# intents required
 intents = discord.Intents.all()
-client = commands.Bot(command_prefix="?", help_command=None, intents=intents)
+
+BOT_PREFIX = '-'
 
 
-@client.event
-async def on_ready():
-    print(f"Bot {client.user} is running!")
-    for cog in cogs:
+class ValorantBot(commands.Bot):
+    debug: bool
+    bot_app_info: discord.AppInfo
+
+    def __init__(self) -> None:
+        self.bot = super().__init__(command_prefix=BOT_PREFIX, case_insensitive=True, intents=intents)
+        self.session: aiohttp.ClientSession = None
+        self.bot_version = '1.2.3'
+        self.tree.interaction_check = self.interaction_check
+
+    @staticmethod
+    async def interaction_check(interaction: discord.Interaction) -> bool:
+        locale_v2.set_interaction_locale(interaction.locale)  # bot responses localized # wait for update
+        locale_v2.set_valorant_locale(interaction.locale)  # valorant localized
+        return True
+
+    @property
+    def owner(self) -> discord.User:
+        return self.bot_app_info.owner
+
+    async def on_ready(self) -> None:
+        await self.tree.sync()
+        print(f"\nLogged in as: {self.user}\n\n BOT IS READY !")
+        print(f"Version: {self.bot_version}")
+
+        # bot presence
+        activity_type = discord.ActivityType.listening
+        await self.change_presence(activity=discord.Activity(type=activity_type, name="ограбление"))
+
+    async def setup_hook(self) -> None:
+        if self.session is None:
+            self.session = aiohttp.ClientSession()
+
         try:
-            print(f"Loading cog {cog}")
-            await client.load_extension(cog)
-            print(f"Loaded cog {cog}")
-        except Exception as e:
-            exc = f"{type(e).__name__}: {e}"
-            print(f"Failed to load cog {cog}\n{exc}")
+            self.owner_id = int(os.getenv('OWNER_ID'))
+        except ValueError:
+            self.bot_app_info = await self.application_info()
+            self.owner_id = self.bot_app_info.owner.id
 
-    qty_members = 0
-    for guild in client.guilds:
-        qty_members += len(guild.members)
-        client.tree.clear_commands(guild=guild)
+        self.setup_cache()
+        # self.get_backup()
+        await self.load_cogs()
+        # await self.tree.sync()
 
-    await message(client)
+    async def load_cogs(self) -> None:
+        for ext in initial_extensions:
+            try:
+                await self.load_extension(ext)
+            except (
+                    ExtensionNotFound,
+                    NoEntryPointError,
+                    ExtensionFailed,
+            ):
+                print(f'Failed to load extension {ext}.', file=sys.stderr)
+                traceback.print_exc()
 
-    await client.change_presence(
-        status=discord.Status.online,
-        activity=discord.Activity(
-            name=f'мониторю {qty_members} джабджиков'
-        )
-    )
+    @staticmethod
+    def setup_cache() -> None:
+        try:
+            open('data/cache.json')
+        except FileNotFoundError:
+            get_cache()
 
-    await client.tree.sync()
+    @staticmethod
+    def get_backup() -> None:
+        fetch_user()
+        fetch_notify()
+
+    async def close(self) -> None:
+        await self.session.close()
+        await super().close()
+
+    async def start(self, debug: bool = False) -> None:
+        self.debug = debug
+        return await super().start(os.getenv('TOKEN'), reconnect=True)
 
 
-client.run(cfg.bot.token)
+def run_bot() -> None:
+    bot = ValorantBot()
+    asyncio.run(bot.start())
+
+
+if __name__ == '__main__':
+    run_bot()
